7f89c617162c5db5720c2ee5c21532fbca4aab0a
Amfmac <you@example.com>
2023-11-13 00:42:24 +0000

    -

--- Triangle.Compiler/src/main/java/triangle/codeGenerator/Encoder.java
+++ Triangle.Compiler/src/main/java/triangle/codeGenerator/Encoder.java
@@ -135,13 +135,13 @@ public final class Encoder implements ActualParameterVisitor<Frame, Integer>,
 
 		var jumpAddr = emitter.emit(OpCode.JUMP, 0, Register.CB, 0);
 		var loopAddr = emitter.getNextInstrAddr();
-		var jumpifAddr = emitter.emit(OpCode.JUMPIF, Machine.falseRep, Register.CB, 0);
+		//var jumpifAddr = emitter.emit(OpCode.JUMPIF, Machine.falseRep, Register.CB, 0);
 
 		ast.C1.visit(this, frame); // order for loopWhile starts with C1 block
 		emitter.patch(jumpAddr); // jumps to Expression
 
 		ast.E.visit(this, frame); // next in order Expression
-		emitter.patch(jumpifAddr); // jumps relative to if expression successful to C2 block
+		//emitter.patch(jumpifAddr); // jumps relative to if expression successful to C2 block
 
 		ast.C2.visit(this, frame); // then the C2 'do' block
 		emitter.emit(OpCode.JUMPIF, Machine.trueRep, Register.CB, loopAddr); // once C2 block has been executed, jump back to start of loop
