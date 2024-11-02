13068c5f591753e1f713e74b15b217797eae3edb
Zaraksh Rahman <zarakshR@pm.me>
2024-11-02 04:04:16 +0000

    move PrettyPrinter to toplevel

--- Triangle.Compiler/src/main/java/triangle/PrettyPrinter.java
+++ Triangle.Compiler/src/main/java/triangle/PrettyPrinter.java
@@ -1,9 +1,12 @@
-package triangle.ast;
+package triangle;
+
+import triangle.ast.Program;
+import triangle.ast.ProgramVisitor;
 
 public final class PrettyPrinter implements ProgramVisitor {
 
     @Override public void visit(final Program program) {
-
+        visit(program.statements());
     }
 
 }
