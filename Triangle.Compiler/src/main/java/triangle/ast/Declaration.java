543b943ab6d9d2574b1cebb0fa8789ea8a68068e
Zaraksh Rahman <zarakshR@pm.me>
2024-11-03 03:54:01 +0000

    Declaration: getName

--- Triangle.Compiler/src/main/java/triangle/ast/Declaration.java
+++ Triangle.Compiler/src/main/java/triangle/ast/Declaration.java
@@ -6,19 +6,45 @@ sealed public interface Declaration
         permits Declaration.ConstDeclaration, Declaration.FuncDeclaration, Declaration.TypeDeclaration,
                 Declaration.VarDeclaration {
 
-    interface Visitor<ST, T,E extends Exception> {
+    String getName();
+
+    interface Visitor<ST, T, E extends Exception> {
 
         T visit(ST state, Declaration declaration) throws E;
 
     }
 
-    record ConstDeclaration(String name, Expression value) implements Declaration { }
+    record ConstDeclaration(String name, Expression value) implements Declaration {
 
-    record VarDeclaration(String name, Type type) implements Declaration { }
+        @Override public String getName() {
+            return name;
+        }
+
+    }
+
+    record VarDeclaration(String name, Type type) implements Declaration {
+
+        @Override public String getName() {
+            return name;
+        }
+
+    }
 
     record FuncDeclaration(String func, List<Parameter> parameters, Type returnType, Statement expression)
-            implements Declaration { }
+            implements Declaration {
 
-    record TypeDeclaration(String name, Type type) implements Declaration { }
+        @Override public String getName() {
+            return func;
+        }
+
+    }
+
+    record TypeDeclaration(String name, Type type) implements Declaration {
+
+        @Override public String getName() {
+            return name;
+        }
+
+    }
 
 }
