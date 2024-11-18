0b2a7c56fa6748bb95b16f352577121b64be9c69
Zaraksh Rahman <zarakshR@pm.me>
2024-11-18 21:10:21 +0000

    Implement SemanticAnalyzerTest

--- Triangle.Compiler/src/test/java/triangle/analysis/SemanticAnalyzerTest.java
+++ Triangle.Compiler/src/test/java/triangle/analysis/SemanticAnalyzerTest.java
@@ -1,4 +1,70 @@
 package triangle.analysis;
 
+import org.junit.jupiter.api.Test;
+import triangle.parsing.Parser;
+import triangle.parsing.SyntaxError;
+
+import java.io.IOException;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static triangle.util.TestUtils.inputStreamOf;
+
 public class SemanticAnalyzerTest {
+    static {
+        // TODO: we should use proper logging in the rest of the code and get rid of this
+        System.setErr(new PrintStream(PrintStream.nullOutputStream()));
+    }
+
+    private static boolean analyzeExpectException(String code, Class<? extends SemanticException> excClass)
+    throws SyntaxError, IOException {
+        Parser parser = new Parser(inputStreamOf(code));
+        SemanticAnalyzer semanticAnalyzer = new SemanticAnalyzer();
+
+        semanticAnalyzer.analyzeAndType(parser.parseProgram());
+
+        for (SemanticException exc : semanticAnalyzer.getErrors()) {
+            // exc instance <Expected Exception Class>
+            if (excClass.equals(exc.getClass())) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    @Test public void testIntLitTooLarge() throws SyntaxError, IOException {
+        assertTrue(analyzeExpectException("99999", SemanticException.IntegerLiteralTooLarge.class));
+    }
+
+    @Test public void testDuplicateRecordField() throws SyntaxError, IOException {
+        assertTrue(analyzeExpectException("{ a ~ 10, a ~ 20}", SemanticException.DuplicateRecordField.class));
+    }
+
+    @Test public void testAssignmentToConstant() throws SyntaxError, IOException {
+        assertTrue(analyzeExpectException("let const x ~ 10 in x := 1", SemanticException.AssignmentToConstant.class));
+    }
+
+    @Test public void testDuplicateDeclaration() throws SyntaxError, IOException {
+        assertTrue(analyzeExpectException("let var x : Integer; var x : Integer in x",
+                                          SemanticException.DuplicateDeclaration.class
+        ));
+    }
+
+    @Test public void testDuplicateParameter() throws SyntaxError, IOException {
+        assertTrue(analyzeExpectException("let proc p(x : Integer, x : Integer) ~ puteol() in 1",
+                                          SemanticException.DuplicateParameter.class
+        ));
+    }
+
+    @Test public void testUndeclaredUse() throws SyntaxError, IOException {
+        assertTrue(analyzeExpectException("x", SemanticException.UndeclaredUse.class));
+    }
+
+    @Test public void testInvalidArgument() throws SyntaxError, IOException {
+        assertTrue(analyzeExpectException("let proc p(var x : Integer) ~ puteol(); var x : Integer in p(x)",
+                                          SemanticException.InvalidArgument.class
+        ));
+    }
+
 }
