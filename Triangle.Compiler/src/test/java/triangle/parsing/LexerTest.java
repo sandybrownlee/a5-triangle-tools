05a92c0eec54c272bce1c08821002e33313474a4
Zaraksh Rahman <zarakshR@pm.me>
2024-11-18 19:33:36 +0000

    implement Lexer tests

--- Triangle.Compiler/src/test/java/triangle/parsing/LexerTest.java
+++ Triangle.Compiler/src/test/java/triangle/parsing/LexerTest.java
@@ -1,61 +1,92 @@
 package triangle.parsing;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
-import triangle.Compiler;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LexerTest {
 
-    // TODO: this test is harmful, we dont want to test for IOExceptions, test instead for whether or not lexer lexes
-    //  identifiers etc correctly
-    //@formatter:off
-    @ValueSource(strings = {
-            "/adddeep.tri",
-            "/arrays.tri",
-            "/assignments.tri",
-            "/bank.tri",
-            "/bardemo.tri",
-            "/complexrecord.tri",
-            "/control.tri",
-            "/deepnest.tri",
-            "/directories.tri",
-            "/errors.tri",
-            "/every.tri",
-            "/factorials.tri",
-            "/foldable.tri",
-            "/functions.tri",
-            "/hi.tri",
-            "/hi-newcomment.tri",
-            "/hi-newcomment2.tri",
-            "/hoistable.tri",
-            "/hullo.tri",
-            "/ifdemo.tri",
-            "/increment.tri",
-            "/loopwhile.tri",
-            "/names.tri",
-            "/nesting.tri",
-            "/procedural.tri",
-            "/procedures.tri",
-            "/procparam.tri",
-            "/records.tri",
-            "/repeatuntil.tri",
-            "/repl.tri",
-            "/simpleadding.tri",
-            "/triangle.tri",
-            "/unaryops.tri",
-            "/varinvar.tri",
-            "/while.tri",
-            "/while-longloop.tri",
-    })
-    //@formatter:on
-    @ParameterizedTest public void testLex(String filename) {
-        Lexer lexer = new Lexer(Compiler.class.getResourceAsStream(filename));
+    // generates an ad-hoc input stream that reads from a snippet of provided source
+    private static InputStream inputStreamOf(String sourceCode) {
+        return new InputStream() {
+            private int index = 0;
 
-        // lexer shouldn't throw exceptions
-        Assertions.assertDoesNotThrow(() -> {
-            while (lexer.nextToken().getKind() != Token.Kind.EOT) { }
-        });
+            @Override public int read() {
+                if (index == sourceCode.length()) {
+                    return -1;
+                }
+
+                return sourceCode.charAt(index++);
+            }
+        };
     }
 
+    @Test public void testTextToken() throws IOException {
+        Lexer lexer = new Lexer(inputStreamOf("abcd1234"));
+
+        TextToken t = (TextToken) lexer.nextToken();
+
+        assertEquals(Token.Kind.IDENTIFIER, t.getKind());
+        assertEquals("abcd1234", t.getText());
+        assertEquals(Token.Kind.EOT, lexer.nextToken().getKind());
+    }
+
+    @Test public void testToken() throws IOException {
+        Lexer lexer = new Lexer(inputStreamOf("while"));
+
+        assertEquals(Token.Kind.WHILE, lexer.nextToken().getKind());
+    }
+
+    // lexer should EOT repeatedly after end of transmission
+    @Test public void testEOT() throws IOException {
+        Lexer lexer = new Lexer(inputStreamOf(""));
+
+        // only check repeated EOTs 10 times, this is probably good enough for anything we might want to do with Lexer
+        for (int i = 0; i < 10; i++) {
+            assertEquals(Token.Kind.EOT, lexer.nextToken().getKind());
+        }
+    }
+
+    // lexer should have maximal munch
+    @Test public void testMaximalMunching() throws IOException {
+        Lexer lexer = new Lexer(inputStreamOf("whileeeee"));
+
+        assertEquals(Token.Kind.IDENTIFIER, lexer.nextToken().getKind());
+    }
+
+    @Test public void testOperator() throws IOException {
+        Lexer lexer = new Lexer(inputStreamOf("-%$!@#"));
+
+        assertEquals(Token.Kind.OPERATOR, lexer.nextToken().getKind());
+    }
+
+    // lexer must split tokens at whitespace
+    @Test public void testWhitespaceSplitting() throws IOException {
+        Lexer lexer = new Lexer(inputStreamOf("while if abcd123"));
+
+        assertEquals(Token.Kind.WHILE, lexer.nextToken().getKind());
+        assertEquals(Token.Kind.IF, lexer.nextToken().getKind());
+        assertEquals(Token.Kind.IDENTIFIER, lexer.nextToken().getKind());
+    }
+
+    // test line no.
+    @Test public void testLineNo() throws IOException {
+        Lexer lexer = new Lexer(inputStreamOf("line1\nline2\nline3"));
+
+        assertEquals(1, lexer.nextToken().getLine());
+        assertEquals(2, lexer.nextToken().getLine());
+        assertEquals(3, lexer.nextToken().getLine());
+    }
+
+    // test col. no.
+    @Test public void testColNo() throws IOException {
+        Lexer lexer = new Lexer(inputStreamOf("abcd abcd abcd"));
+
+        assertEquals(1, lexer.nextToken().getColumn());
+        assertEquals(6, lexer.nextToken().getColumn());
+        assertEquals(11, lexer.nextToken().getColumn());
+    }
 }
