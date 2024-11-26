340ef8787035c0098ef9e47ba2d812c1176d446d
Rithwik <rithwik@delhipeople.com>
2024-11-26 20:11:13 +0000

    End of task 3

--- Triangle.Compiler/src/test/java/triangle/syntacticAnalyser/TestScanner.java
+++ Triangle.Compiler/src/test/java/triangle/syntacticAnalyser/TestScanner.java
@@ -15,7 +15,7 @@ import triangle.syntacticAnalyzer.Scanner;
 import triangle.syntacticAnalyzer.SourceFile;
 
 public class TestScanner {
-	
+
 	/* some individual unit tests for helper methods in Scanner */
 
 	@Test
@@ -31,7 +31,7 @@ public class TestScanner {
 		assertFalse(Scanner.isDigit(';'));
 		assertFalse(Scanner.isDigit('\n'));
 	}
-	
+
 	@Test
 	public void testIsOperator() {
 		assertTrue(Scanner.isOperator('*'));
@@ -45,59 +45,68 @@ public class TestScanner {
 		assertFalse(Scanner.isOperator(';'));
 		assertFalse(Scanner.isOperator('\n'));
 	}
-	
-	
+
+
 	/* these tests all try to compile example programs... */
-	
+
 	@Test
 	public void testHi() {
 		compileExpectSuccess("/hi.tri");
 	}
-	
+
 
 	@Test
 	public void testHiNewComment() {
 		compileExpectFailure("/hi-newcomment.tri");
 	}
-	
+
 
 	@Test
 	public void testHiNewComment2() {
 		compileExpectFailure("/hi-newcomment2.tri");
 	}
-	
+
 
 	@Test
 	public void testBarDemo() {
 		compileExpectFailure("/bardemo.tri");
 	}
-	
+
 
 	@Test
 	public void testRepeatUntil() {
 		compileExpectFailure("/repeatuntil.tri");
 	}
-	
-	
-	
+
+
+    /**
+     * Test that the square shotcut command (a**) compiles and runs
+     */
+    @Test
+	public void testSquareShortcutCommand() {
+		compileExpectSuccess("/square.tri");
+	}
+
+
+
 	private void compileExpectSuccess(String filename) {
 		// build.gradle has a line sourceSets.test.resources.srcDir file("$rootDir/programs")
 		// which adds the programs directory to the list of places Java can easily find files
-		// getResource() below searches for a file, which is in /programs 
+		// getResource() below searches for a file, which is in /programs
 		//SourceFile source = SourceFile.ofPath(this.getClass().getResource(filename).getFile().toString());
 		SourceFile source = SourceFile.fromResource(filename);
-		
+
 		Scanner scanner = new Scanner(source);
 		ErrorReporter reporter = new ErrorReporter(true);
 		Parser parser = new Parser(scanner, reporter);
-		
+
 		parser.parseProgram();
-		
+
 		// we should get to here with no exceptions
-		
+
 		assertEquals("Problem compiling " + filename, 0, reporter.getNumErrors());
 	}
-	
+
 	private void compileExpectFailure(String filename) {
 		//SourceFile source = SourceFile.ofPath(this.getClass().getResource(filename).getFile().toString());
 		SourceFile source = SourceFile.fromResource(filename);
@@ -111,7 +120,7 @@ public class TestScanner {
 				parser.parseProgram();
 			}
 		});
-		
+
 		// currently this program will fail
 		assertNotEquals("Problem compiling " + filename, 0, reporter.getNumErrors());
 	}
