c428ca70e15e7a1aabe3d4b24859ff14879b7fea
RobertNadin <nadinrobert27@gmail.com>
2024-11-28 21:27:03 +0000

    task 4

--- Triangle.Compiler/src/test/java/triangle/syntacticAnalyser/TestScanner.java
+++ Triangle.Compiler/src/test/java/triangle/syntacticAnalyser/TestScanner.java
@@ -15,7 +15,7 @@ import triangle.syntacticAnalyzer.Scanner;
 import triangle.syntacticAnalyzer.SourceFile;
 
 public class TestScanner {
-	
+
 	/* some individual unit tests for helper methods in Scanner */
 
 	@Test
@@ -31,89 +31,98 @@ public class TestScanner {
 		assertFalse(Scanner.isDigit(';'));
 		assertFalse(Scanner.isDigit('\n'));
 	}
-	
+
 	@Test
 	public void testIsOperator() {
+		// Existing tests
 		assertTrue(Scanner.isOperator('*'));
 		assertTrue(Scanner.isOperator('/'));
 		assertTrue(Scanner.isOperator('?'));
 		assertTrue(Scanner.isOperator('+'));
 		assertTrue(Scanner.isOperator('-'));
-		assertFalse(Scanner.isOperator('a'));
-		assertFalse(Scanner.isOperator('Z'));
-		assertFalse(Scanner.isOperator('1'));
-		assertFalse(Scanner.isOperator(';'));
-		assertFalse(Scanner.isOperator('\n'));
+
+		// Additional tests for edge cases
+		assertTrue(Scanner.isOperator('='));  // '=' is an operator
+		assertTrue(Scanner.isOperator('<'));  // '<' is an operator
+		assertTrue(Scanner.isOperator('>'));  // '>' is an operator
+		assertTrue(Scanner.isOperator('&'));  // '&' is an operator
+
+		assertFalse(Scanner.isOperator('a'));  // 'a' is not an operator
+		assertFalse(Scanner.isOperator('Z'));  // 'Z' is not an operator
+		assertFalse(Scanner.isOperator('1'));  // '1' is not an operator
+		assertFalse(Scanner.isOperator(';'));  // ';' is not an operator
+		assertFalse(Scanner.isOperator('\n')); // Newline is not an operator
+		assertFalse(Scanner.isOperator('@'));  // '@' is not an operator
 	}
-	
-	
+
+	@Test
+	public void testIsLetter() {
+		// Test for all alphabetic characters
+		for (char c = 'a'; c <= 'z'; c++) {
+			assertTrue(Scanner.isLetter(c));  // Lowercase letters should be true
+		}
+		for (char c = 'A'; c <= 'Z'; c++) {
+			assertTrue(Scanner.isLetter(c));  // Uppercase letters should be true
+		}
+
+		// Test for non-letter characters
+		assertFalse(Scanner.isLetter('1'));  // Digits should be false
+		assertFalse(Scanner.isLetter('@'));  // Symbols should be false
+		assertFalse(Scanner.isLetter('*'));  // Special characters should be false
+		assertFalse(Scanner.isLetter(';'));  // Punctuation should be false
+		assertFalse(Scanner.isLetter('\n')); // Newline should be false
+	}
+
 	/* these tests all try to compile example programs... */
-	
-	/*@Test
-	public void testHi() {
-		compileExpectSuccess("/hi.tri");
-	}
-	*/
+
+    /*@Test
+    public void testHi() {
+        compileExpectSuccess("/hi.tri");
+    }
+    */
 
 	@Test
 	public void testHiNewComment() {
 		compileExpectFailure("/hi-newcomment.tri");
 	}
-	
 
 	@Test
 	public void testHiNewComment2() {
 		compileExpectFailure("/hi-newcomment2.tri");
 	}
-	
 
 	@Test
 	public void testBarDemo() {
 		compileExpectFailure("/bardemo.tri");
 	}
-	
 
 	@Test
 	public void testRepeatUntil() {
 		compileExpectFailure("/repeatuntil.tri");
 	}
-	
-	
-	
+
 	private void compileExpectSuccess(String filename) {
-		// build.gradle has a line sourceSets.test.resources.srcDir file("$rootDir/programs")
-		// which adds the programs directory to the list of places Java can easily find files
-		// getResource() below searches for a file, which is in /programs 
-		//SourceFile source = SourceFile.ofPath(this.getClass().getResource(filename).getFile().toString());
-		SourceFile source = SourceFile.fromResource(filename);
-		
-		Scanner scanner = new Scanner(source);
-		ErrorReporter reporter = new ErrorReporter(true);
-		Parser parser = new Parser(scanner, reporter);
-		
-		parser.parseProgram();
-		
-		// we should get to here with no exceptions
-		
-		assertEquals("Problem compiling " + filename, 0, reporter.getNumErrors());
-	}
-	
-	private void compileExpectFailure(String filename) {
-		//SourceFile source = SourceFile.ofPath(this.getClass().getResource(filename).getFile().toString());
 		SourceFile source = SourceFile.fromResource(filename);
 		Scanner scanner = new Scanner(source);
 		ErrorReporter reporter = new ErrorReporter(true);
 		Parser parser = new Parser(scanner, reporter);
 
-		// we expect an exception here as the program has invalid syntax
+		parser.parseProgram();
+		assertEquals("Problem compiling " + filename, 0, reporter.getNumErrors());
+	}
+
+	private void compileExpectFailure(String filename) {
+		SourceFile source = SourceFile.fromResource(filename);
+		Scanner scanner = new Scanner(source);
+		ErrorReporter reporter = new ErrorReporter(true);
+		Parser parser = new Parser(scanner, reporter);
+
 		assertThrows(RuntimeException.class, new ThrowingRunnable() {
-			public void run(){
+			public void run() {
 				parser.parseProgram();
 			}
 		});
-		
-		// currently this program will fail
+
 		assertNotEquals("Problem compiling " + filename, 0, reporter.getNumErrors());
 	}
-
 }
