914532161de78140523b2b3d26ce61d44cf1148c
iamimpulse08 <Kieranwalmsley0002@gmail.com>
2024-11-28 16:11:53 +0000

    Multiple loop invariant support added

--- Triangle.Compiler/src/main/java/triangle/optimiser/HoistVisitor.java
+++ Triangle.Compiler/src/main/java/triangle/optimiser/HoistVisitor.java
@@ -3,25 +3,11 @@ package triangle.optimiser;
 import triangle.StdEnvironment;
 import triangle.abstractSyntaxTrees.AbstractSyntaxTree;
 import triangle.abstractSyntaxTrees.Program;
-import triangle.abstractSyntaxTrees.actuals.*;
-import triangle.abstractSyntaxTrees.aggregates.MultipleArrayAggregate;
-import triangle.abstractSyntaxTrees.aggregates.MultipleRecordAggregate;
-import triangle.abstractSyntaxTrees.aggregates.SingleArrayAggregate;
-import triangle.abstractSyntaxTrees.aggregates.SingleRecordAggregate;
 import triangle.abstractSyntaxTrees.commands.*;
 import triangle.abstractSyntaxTrees.declarations.*;
 import triangle.abstractSyntaxTrees.expressions.*;
-import triangle.abstractSyntaxTrees.formals.*;
-import triangle.abstractSyntaxTrees.terminals.CharacterLiteral;
 import triangle.abstractSyntaxTrees.terminals.Identifier;
-import triangle.abstractSyntaxTrees.terminals.IntegerLiteral;
-import triangle.abstractSyntaxTrees.terminals.Operator;
-import triangle.abstractSyntaxTrees.types.*;
-import triangle.abstractSyntaxTrees.visitors.*;
-import triangle.abstractSyntaxTrees.vnames.DotVname;
 import triangle.abstractSyntaxTrees.vnames.SimpleVname;
-import triangle.abstractSyntaxTrees.vnames.SubscriptVname;
-import triangle.abstractSyntaxTrees.vnames.Vname;
 import triangle.contextualAnalyzer.IdentificationTable;
 import triangle.syntacticAnalyzer.SourcePosition;
 
@@ -34,6 +20,7 @@ public class HoistVisitor extends ConstantFolder {
 	private boolean currentlyInLoop = false;
 	private int numberOfConstants = 0;
 	Stack<AbstractSyntaxTree> stack = new Stack<>();
+	Stack<LetCommand> letStack = new Stack<>();
 
 	@Override
 	public AbstractSyntaxTree visitSimpleVname(SimpleVname ast, Void arg) {
@@ -41,14 +28,20 @@ public class HoistVisitor extends ConstantFolder {
 		return ast;
 	}
 
+	/**
+	 * Opens the scope of the identification table present within this visitor,
+	 * Visits the AST which collects variables to update and submits a
+	 *
+	 * @param ast
+	 * @param arg
+	 * @return
+	 */
 	@Override
 	public AbstractSyntaxTree visitProgram(Program ast, Void arg) {
 		idTable.openScope();
 		ast.C.visit(this);
-
 		currentlyHoisting = true;
 		ast.C.visit(this);
-		//idTable.closeScope(); needed? y/n?
 		return null;
 	}
 
@@ -57,6 +50,7 @@ public class HoistVisitor extends ConstantFolder {
 		AbstractSyntaxTree replacement1 = ast.E1.visit(this);
 		AbstractSyntaxTree replacement2 = ast.E2.visit(this);
 		ast.O.visit(this);
+
 		if (currentlyHoisting && currentlyInLoop) {
 			if (canHoistExpression(replacement1) || canHoistExpression(replacement2)) {
 				return ast;
@@ -115,7 +109,8 @@ public class HoistVisitor extends ConstantFolder {
 
 				idTable.enter(spelling, d);
 			}
-		} else if (currentlyHoisting && currentlyInLoop) {
+		}
+		else if (currentlyHoisting && currentlyInLoop) {
 			// nullability check as binary expression evaluation can and will return null if there is no invariant present in the current assignment.
 			if (replacement != null) {
 				// logic for creating an expression to hoist and pushing to the stack.
@@ -161,13 +156,11 @@ public class HoistVisitor extends ConstantFolder {
 		if (currentlyHoisting) {
 			if (node2 instanceof WhileCommand) {
 				SourcePosition pos = ast.getPosition();
+				Command sequentialSubTreeToInject = createSequentialCommand(pos);
+				// we now have a let command and access to the while command, to place these together we wrap them in a sequential command
+				// we now set the parent tree equal to the combined let trees in sequential command form
+				sequentialSubTreeToInject = new SequentialCommand(sequentialSubTreeToInject, ast.C2, pos);
 
-				// grab the statement to hoist from the top of the stack
-				// this assignment command should be something like "temp" = C + 2 in the while hoist .tri program
-				AssignCommand assignmentToInject = (AssignCommand) stack.pop();
-
-				// To create a let command, we need a variable declaration, which we can create from the assignment command
-				SequentialCommand sequentialSubTreeToInject = getTreeToInject(ast, assignmentToInject, pos);
 
 				// and then to add this back to the tree, we pass a new sequential command which contains the updated subtree
 				// and the rest of the tree on the left as we're doing DFS algorithms to figure out the program layout.
@@ -177,17 +170,33 @@ public class HoistVisitor extends ConstantFolder {
 		return null;
 	}
 
-	private static SequentialCommand getTreeToInject(SequentialCommand ast, AssignCommand assignmentToInject, SourcePosition pos) {
+	public LetCommand createLetCommand() {
+		// grab the statement to hoist from the top of the stack
+		// this assignment command should be something like "temp" = C + 2 in the while hoist .tri program
+		if (stack.isEmpty()) {
+			return null;
+		}
+		AssignCommand assignmentToInject = (AssignCommand) stack.pop();
+
+		// To create a let command, we need a variable declaration, which we can create from the assignment command
 		SimpleVname sAST = (SimpleVname) assignmentToInject.V;
 		Identifier id = sAST.I;
 		// we create a variable, "temp" for example, and then wrap it inside a let command.
-		VarDeclaration var = new VarDeclaration(id, StdEnvironment.integerType, pos);
-		LetCommand letAST = new LetCommand(var, assignmentToInject, pos);
-
-		// we now have a let command and access to the while command, to place these together we wrap them in a sequential command
-		SequentialCommand sequentialSubTreeToInject = new SequentialCommand(letAST, ast.C2, pos);
-		return sequentialSubTreeToInject;
+		VarDeclaration var = new VarDeclaration(id, StdEnvironment.integerType, assignmentToInject.getPosition());
+		return new LetCommand(var, assignmentToInject, assignmentToInject.getPosition());
 	}
+
+	public Command createSequentialCommand(SourcePosition pos) {
+
+		LetCommand letCommand = createLetCommand();
+		if (letCommand != null) {
+			return new SequentialCommand(createSequentialCommand(pos), letCommand, pos);
+		}
+		else {
+			return new EmptyCommand(pos);
+		}
+	}
+
 
 	@Override
 	public AbstractSyntaxTree visitWhileCommand(WhileCommand ast, Void arg) {
